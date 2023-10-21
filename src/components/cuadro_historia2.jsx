61147ab3862f868e0bd688ca2bcdff876a965a67
Diego Veas <diego81@hotmail.cl>
2023-10-21 20:27:03 -0300

    Cuadros para Historia v2

--- src/components/cuadro_historia2.jsx
+++ src/components/cuadro_historia2.jsx
@@ -4,7 +4,8 @@ const OneRectangle2 = () => {
   // Define an array of colors
   const color = ["#3a72a9"];
   const text = [
-                'La organización de la Feria de Software está conformada por alumnos, ayudantes y profesores del Departamento de Informática de la Universidad Técnica Federico Santa María del Campus Santiago y Casa Central, que en conjunto integran distintas áreas de trabajo para llevar a cabo el evento. El equipo de Profesores actualmente está formado por: Cecilia Reyes, Lioubov Dombrovskaia, Luis Hevia,Pedro Godoy Cristian Correa. En años anteriores también han participado los profesores Marcello Visconti, Hernán Astudillo, Pablo Cruz y Romina Torres.'
+                'La organización de la Feria de Software está conformada por alumnos, ayudantes y profesores del Departamento de Informática de la Universidad Técnica Federico Santa María del Campus Santiago y Casa Central, que en conjunto integran distintas áreas de trabajo para llevar a cabo el evento.',
+                'El equipo de Profesores actualmente está formado por: Cecilia Reyes, Lioubov Dombrovskaia, Luis Hevia,Pedro Godoy Cristian Correa. En años anteriores también han participado los profesores Marcello Visconti, Hernán Astudillo, Pablo Cruz y Romina Torres.'
                 ];
 
 
