3a876ec1a352780ba72fd4a99731060d7d1d0926
Diego Veas <diego81@hotmail.cl>
2023-10-21 19:01:01 -0300

    Full NavBar en todas las paginas

--- src/pages/historia.jsx
+++ src/pages/historia.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import NavBar from '../components/nav_bar'
-import LogoUSM from '../assets/usm.png'
+import FullNavbar from '../components/full_navbar.jsx'
 
 export const HistoriaPage = () => {
   return (
     <div className='page'>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={LogoUSM} height={100} alt='logo usm' style={{ marginRight: '20px' }} />
-        <h1 className='page__title'>Feria de Software</h1>
-      </div>
-      <NavBar />
+      <FullNavbar/>
       <h2>Historia de la Feria de Software</h2>
     </div>
   )
--- src/pages/proy_anterior.jsx
+++ src/pages/proy_anterior.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import NavBar from '../components/nav_bar'
-import LogoUSM from '../assets/usm.png'
+import FullNavbar from '../components/full_navbar.jsx'
 
 import { Button } from '@mui/material'
 
 export const AnteriorPage = () => {
   return (
     <div className='page'>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={LogoUSM} height={100} alt='logo usm' style={{ marginRight: '20px' }} />
-        <h1 className='page__title'>Feria de Software</h1>
-      </div>
-      <NavBar />
+      <FullNavbar/>
       <h2>Proyectos de la Feria de Software de ediciones anteriores</h2>
       <Button variant="contained">button</Button> 
     </div>
