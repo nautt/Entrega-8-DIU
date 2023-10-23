c60d03959859a8a33f2ac0c8b251be0aebd833bf
prettydoggo <52218179+nautt@users.noreply.github.com>
2023-10-22 21:19:11 -0300

    Agregada funcionalidad a pagina de registro
    Botones funcionales para elegir numero de asistentes y simulacion de carga al enviar solicitud

--- src/components/app_router.jsx
+++ src/components/app_router.jsx
@@ -7,6 +7,7 @@ import AuspiPage from '../pages/auspiciadores'
 import ActualPage from '../pages/proy_actual'
 import AnteriorPage from '../pages/proy_anterior'
 import RegisterPage from '../pages/register'
+import LoadingPage from '../pages/loading_page'
 
 
 const AppRouter = () => {
@@ -19,6 +20,7 @@ const AppRouter = () => {
         <Route path='/proy_actual' element={<ActualPage/>} />
         <Route path='/proy_anterior' element={<AnteriorPage/>} />
         <Route path='/register_page' element={<RegisterPage/>} />
+        <Route path='/register_result' element={<LoadingPage/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
 import Custom_Navbar from '../components/navbar.jsx'
 
 export const RegisterPage = () => {
+  const [counterEntradas, setCounter] = useState(1);
+  const [startLoading, setStartLoading] = useState(false);
+  const min_entradas = 1;
+  const max_entradas = 5;
+  const navigate = useNavigate();
+
+  //Agregar entrada
+  const increase = () => {
+    setCounter(count => (count < max_entradas) ? count+1 : count);
+  };
+
+  //eliminar entrada
+  const decrease = () => {
+    setCounter(count => (count > min_entradas) ? count-1 : count);
+  };
+
+  const registerLoading = () => {
+    setStartLoading(true);
+
+    const timer = setTimeout(() => {
+      console.log("aaaaa");
+      navigate('/register_result');
+    }, 4000);
+  }
+
   return (
     <>
       <Custom_Navbar/>
@@ -17,49 +44,59 @@ export const RegisterPage = () => {
           <Card className="form-container">
             <Card.Body className='register-card-body'>
               <Card.Title className='register-card-title'>Ingresa tus datos</Card.Title>
-              {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
               <Form>
                 <Form.Group className="mb-3" controlId="formFullName">
                   <Row>
                     <Col>
                       <Form.Label>Nombre</Form.Label>
-                      <Form.Control type="text" placeholder="Ingresa tu nombre" />
+                      <Form.Control type="text" placeholder="Ingresa tu nombre" required/>
                     </Col>  
                     <Col>
                       <Form.Label>Apellido</Form.Label>
-                      <Form.Control type="text" placeholder="Ingresa tu nombre" />
+                      <Form.Control type="text" placeholder="Ingresa tu nombre" required/>
                     </Col>
                   </Row>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Label>Email (Aquí se enviarán tus entradas)</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                 <Form.Label>Tickets</Form.Label>
                   <InputGroup>
-                  <Button variant="outline-secondary">-</Button>
+                  <Button variant="outline-secondary" onClick={decrease}>-</Button>
                     <Form.Control
                       type='number'
-                      min={1}
-                      max={5}
+                      min={min_entradas}
+                      max={max_entradas}
                       placeholder="# de tickets"
-                      defaultValue={1}
-                      aria-label="Recipient's username with two button addons"
+                      value={counterEntradas}
+                      readOnly
                     />
-                    <Button variant="outline-secondary">+</Button>
+                    <Button variant="outline-secondary" onClick={increase}>+</Button>
                   </InputGroup>
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
-                  Registrar
-                </Button>
+                {!startLoading && 
+                  <Button variant="primary" type="submit" onClick={registerLoading}>
+                    Registrar entradas
+                  </Button>
+                }
+                {startLoading &&
+                  <Button variant="primary" href='/register_result' disabled>
+                    <Spinner
+                      as="span"
+                      animation="grow"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    Espere...
+                  </Button>
+                }
               </Form>
-
-              {/* <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link> */}
             </Card.Body>
           </Card>
         </div>
